07cf098263edb4e80435bc585fc77916f26725ab
deshpandeanoop <anoop.bhardwaja@gmail.com>
2021-08-11 00:44:44 +0530

    1. Implemented upsert operation 2. Implemented incremental query operation

---
+++ src/main/scala/com/explore/apache/constants/HudiColumns.scala
@@ -0,0 +1,10 @@
+package com.explore.apache.constants
+
+/**
+ * <code>HudiColumns</code> component all standard columns added
+ * to any hudi table by the framework.
+ */
+object HudiColumns {
+
+  val HOODIE_COMMIT_TIME = "_hoodie_commit_time"
+}
--- src/main/scala/com/explore/apache/hudi/impl/SeatleLibraryCheckoutDatasetHudiService.scala
+++ src/main/scala/com/explore/apache/hudi/impl/SeatleLibraryCheckoutDatasetHudiService.scala
@@ -1,25 +1,48 @@
 package com.explore.apache.hudi.impl
 
 import com.explore.apache.beans.{LibraryCheckoutIndexInfo, LibraryCheckoutInfo}
-import com.explore.apache.constants.{AppConstants, HudiServiceConstants}
+import com.explore.apache.constants.{AppConstants, HudiColumns, HudiServiceConstants}
 import com.explore.apache.hudi.HudiService
 import com.explore.apache.utils.{HudiServiceHelper, SparkJobHelper}
 import org.apache.hudi.config.HoodieWriteConfig
 import org.apache.hudi.{DataSourceReadOptions, DataSourceWriteOptions, QuickstartUtils}
 import org.apache.spark.sql.{SaveMode, SparkSession}
 
+/**
+ * <code>SeatleLibraryCheckoutDatasetHudiService</code> is <code>HudiService</code>
+ * implementation for Seattle Library checkout info data-set
+ */
 class SeatleLibraryCheckoutDatasetHudiService extends HudiService[LibraryCheckoutInfo] with Serializable {
 
+  /**
+   * Starting point of the program
+   */
   def execute() : Unit = {
     val inputCsvAbsPath = HudiServiceHelper.getInputCsvLoadPath
 
     val sparkSession = SparkJobHelper.getLocalSparkSession("seatle-library-checkout-hudi-service")
 
+    sparkSession.sparkContext.setLogLevel("ERROR")
+
 //    createTableFromCsv(inputCsvAbsPath, HudiServiceConstants.SLC_HUDI_BASE_PATH, HudiServiceConstants.SLC_HUDI_TABLE_NAME, sparkSession)
-    showSnapShotView(HudiServiceConstants.SLC_HUDI_BASE_PATH, sparkSession)
+//    showSnapShotView(HudiServiceConstants.SLC_HUDI_BASE_PATH, sparkSession)
 //    deleteDataFromTable(List.empty, HudiServiceConstants.SLC_HUDI_BASE_PATH, HudiServiceConstants.SLC_HUDI_TABLE_NAME, sparkSession)
+    upsertTable(getDummyUpsertData, HudiServiceConstants.SLC_HUDI_TABLE_NAME, HudiServiceConstants.SLC_HUDI_BASE_PATH, sparkSession)
+   showIncrementalView(HudiServiceConstants.SLC_HUDI_BASE_PATH, sparkSession)
   }
 
+  /**
+   * Creates new hudi table with given Seattle Library checkout info csv extract
+   *
+   * @param inputCsvAbsPath
+   *                Seattle Library extract
+   * @param hudiTableBasePath
+   *                Hudi table base path
+   * @param hudiTableName
+   *                Hudi table name
+   * @param sparkSession
+   *                sparkSession instance of the job
+   */
   override def createTableFromCsv(inputCsvAbsPath: String, hudiTableBasePath: String, hudiTableName: String, sparkSession: SparkSession): Unit = {
     import sparkSession.implicits._
 
@@ -43,6 +66,13 @@ class SeatleLibraryCheckoutDatasetHudiService extends HudiService[LibraryCheckou
       .save(hudiTableBasePath)
   }
 
+  /**
+   * Shows snapshot view of Seattle Library Checkout data-set
+   * @param hudiTablePath
+   *              Hudi table base path
+   * @param sparkSession
+   *              sparkSession instance of the job
+   */
   override def showSnapShotView(hudiTablePath: String, sparkSession: SparkSession): Unit = {
     sparkSession
       .read
@@ -50,12 +80,42 @@ class SeatleLibraryCheckoutDatasetHudiService extends HudiService[LibraryCheckou
       .load(s"${hudiTablePath}/nadvd/*")
       .createOrReplaceTempView(HudiServiceConstants.SLC_HUDI_TEMPORARY_VIEW)
 
-    sparkSession.sql(s"Select bibNumber, itemBarcode, callNumber, itemType, collection from ${HudiServiceConstants.SLC_HUDI_TEMPORARY_VIEW} Where itemBarcode ='0010063255433'")
-      .show(20)
+    sparkSession.sql(s"Select bibNumber, itemBarcode, callNumber, itemType, collection from ${HudiServiceConstants.SLC_HUDI_TEMPORARY_VIEW} Where itemBarcode ='0010063255633'")
+      .show(20, false)
   }
 
-  override def upsertTable(upsertDataset: List[LibraryCheckoutInfo], tableName: String, hudiBasePath: String, sparkSession: SparkSession): Unit = ???
+  /**
+   * Performs upsert operation on seatle library check-in info table
+   *
+   * @param upsertDataset
+   * @param tableName
+   * @param hudiBasePath
+   * @param sparkSession
+   */
+  override def upsertTable(upsertDataset: List[LibraryCheckoutInfo], tableName: String, hudiBasePath: String, sparkSession: SparkSession): Unit = {
+    import sparkSession.implicits._
 
+    sparkSession
+      .createDataset(upsertDataset)
+      .write
+      .format(AppConstants.SPARK_FORMAT_HUDI)
+      .options(QuickstartUtils.getQuickstartWriteConfigs)
+      .option(DataSourceWriteOptions.RECORDKEY_FIELD_OPT_KEY, "bibNumber")
+      .option(DataSourceWriteOptions.PRECOMBINE_FIELD_OPT_KEY, "itemType")
+      .option(DataSourceWriteOptions.PARTITIONPATH_FIELD_OPT_KEY, "collection")
+      .option(HoodieWriteConfig.TABLE_NAME, tableName)
+      .mode(SaveMode.Append)
+      .save(hudiBasePath)
+  }
+
+  /**
+   * Performs delete operation
+   *
+   * @param tobeDeletedDataset
+   * @param hudiBasePath
+   * @param hudiTableName
+   * @param sparkSession
+   */
   override def deleteDataFromTable(tobeDeletedDataset: List[LibraryCheckoutInfo], hudiBasePath: String, hudiTableName: String, sparkSession: SparkSession): Unit = {
     val libraryCheckoutInfo = LibraryCheckoutIndexInfo (
       bibNumber = "2320119",
@@ -77,7 +137,58 @@ class SeatleLibraryCheckoutDatasetHudiService extends HudiService[LibraryCheckou
       .save(hudiBasePath)
   }
 
-  override def showIncrementalView(hudiBaseTablePath: String, sparkSession: SparkSession): Unit = ???
+  /**
+   *
+   * @param hudiBaseTablePath
+   * @param sparkSession
+   */
+  override def showIncrementalView(hudiBaseTablePath: String, sparkSession: SparkSession): Unit = {
+    sparkSession
+      .read
+      .format(AppConstants.SPARK_FORMAT_HUDI)
+      .load(s"${hudiBaseTablePath}/nadvd/*")
+      .createOrReplaceTempView(HudiServiceConstants.SLC_HUDI_TEMPORARY_VIEW)
 
-  override def showPointInTimeView(hudiTablePath: String, sparkSession: SparkSession): Unit = ???
+    import sparkSession.implicits._
+
+    val commits = sparkSession.sql(s"Select distinct(${HudiColumns.HOODIE_COMMIT_TIME}) from ${HudiServiceConstants.SLC_HUDI_TEMPORARY_VIEW} " +
+      s" order by ${HudiColumns.HOODIE_COMMIT_TIME}").map(row => row.getString(0)).take(50)
+
+    println(s"Commits = ${commits.mkString(",")}")
+
+    val beginTimeStamp = commits(commits.length - 2)
+
+    sparkSession
+      .read
+      .format(AppConstants.SPARK_FORMAT_HUDI)
+      .option(DataSourceReadOptions.QUERY_TYPE_OPT_KEY, DataSourceReadOptions.QUERY_TYPE_INCREMENTAL_OPT_VAL)
+      .option(DataSourceReadOptions.BEGIN_INSTANTTIME_OPT_KEY, beginTimeStamp)
+      .load(s"${hudiBaseTablePath}/*/*")
+      .createOrReplaceTempView(HudiServiceConstants.SLC_HUDI_TEMPORARY_VIEW)
+
+    sparkSession
+      .sql(s"Select bibNumber, itemType, collection, callNumber From ${HudiServiceConstants.SLC_HUDI_TEMPORARY_VIEW}")
+      .show(10, false)
+  }
+
+  /**
+   *
+   * @param hudiTablePath
+   * @param sparkSession
+   */
+  override def showPointInTimeView(hudiTablePath: String, sparkSession: SparkSession): Unit = {
+
+  }
+
+  private def getDummyUpsertData : List[LibraryCheckoutInfo] = {
+    List(
+      LibraryCheckoutInfo(
+        bibNumber = "2554635",
+        itemBarcode = "0010063255633",
+        itemType = "acdvd",
+        collection = "nadvd",
+        callNumber = "DVD NARUTO125-129_New"
+      )
+    )
+  }
 }
